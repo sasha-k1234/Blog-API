edc21c2bed1cd83ebfb05890eb5560961ae5bdf8
sasha-1234@ <ja4349950@gmail.com>
2024-04-21 20:39:15 +0200

    MongoDb and mongoose added

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use('/public',express.static(path.join(__dirname,'public')));
+
+const dbUrl = process.env.DB_URL;
+
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"mongoose connection error:"));
+db.once("open",()=>{
+    console.log('DB connected');
+});
 
 app.listen(3000);
