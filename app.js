ab51235698504c5e94c973ce938311d9cda45d14
sasha-1234@ <ja4349950@gmail.com>
2024-04-21 23:01:40 +0200

    ejs set

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const express = require('express');
-require('dotenv').config();
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+require("dotenv").config();
+const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use('/public',express.static(path.join(__dirname,'public')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 const dbUrl = process.env.DB_URL;
-
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
-db.on("error",console.error.bind(console,"mongoose connection error:"));
-db.once("open",()=>{
-    console.log('DB connected');
-});
+db.on("error", console.error.bind(console, "mongoose connection error:"));
+// db.once("open",()=>{
+//     console.log('DB connected');
+// });
 
 app.listen(3000);
